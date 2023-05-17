2202aa8f2ca2feb9f198b498052091b8dc2d9605
MiaTian1024 <sddxtm123@gmail.com>
2023-05-17 23:13:26 +1200

    fix display bug

--- client/script.js
+++ client/script.js
@@ -9,24 +9,18 @@ const date = new Date();
 const currentTime = date.getHours();
 const greeting = document.getElementById('greeting');
 
-greeting.classList.add('fade-in');
-setTimeout(() => {
-  greeting.classList.add('fade-out')
-}, 1000)
-
-
 if(currentTime < 12){
   body.style.backgroundColor = '#50a3a2';
-  greeting.textContent = 'Good Morning';
+  greeting.placeholder = 'Good Morning...';
 }else if(currentTime < 18) {
   body.style.backgroundColor = '#ACB1D6';
-  greeting.textContent = 'Good Afternoon';
+  greeting.placeholder = 'Good Afternoon...';
 }else if(currentTime < 22) {
   body.style.backgroundColor = '#D5B4B4';
-  greeting.textContent = 'Good Evening';
+  greeting.placeholder = 'Good Evening...';
 }else{
   body.style.backgroundColor = '#EDC6B1';
-  greeting.textContent = 'Good Night';
+  greeting.placeholder = 'Good Night...';
 }
 
 
